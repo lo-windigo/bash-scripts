b011825b7135e26dda230ca9cb230b37517a2349
Windigo <jacob@fragdev.com>
2020-03-05 10:30:20 -0800

    Updated wttr.in script to provide full forecast as well

--- wttr.sh
+++ wttr.sh
@@ -2,10 +2,19 @@
 # Get a weather forecast for our local area
 ##
 
+ZIP="${1:-95360}"
+
 # Set up the request URL. See wttr.in/:help for more.
 # Flags:
-# 0 - Only current weather
 # Q - Super quiet version, no weathe report or city/location name
-REQUEST="wttr.in/${1-95360}?0Q"
+
+# Full forecast flag
+# 0 - Only current weather
+FORECAST=
+if [ "$( basename "$0" )" != 'forecast' ]; then
+	FORECAST='0'
+fi
+
+REQUEST="wttr.in/${ZIP}?${FORECAST}Q"
 
 curl -H "Accept-Language: ${LANG%_*}" --compressed "$REQUEST"
