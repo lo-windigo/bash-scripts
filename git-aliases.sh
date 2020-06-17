a653797478bff5b9b66ed3a08275be7ca50c6c1b
Jacob Hume <jacob.hume@tylertech.com>
2020-06-17 11:59:56 -0700

    Update git log alias

--- git-aliases.sh
+++ git-aliases.sh
@@ -11,7 +11,8 @@ git config --global alias.cm 'commit -m'
 git config --global alias.cam 'commit -a -m'
 git config --global alias.co checkout
 git config --global alias.f 'fetch --all'
-git config --global alias.l 'log --graph --date=short --pretty="format:%Cred%h%Cblue%d%Creset %s%n %Cgreen %ad, %an <%ce>%n%Cblue┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"'
+#git config --global alias.l 'log --graph --date=short --pretty="format:%Cred%h%Cblue%d%Creset %s%n %Cgreen %ad, %an <%ce>%n%Cblue┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"'
+git config --global alias.l 'log --graph --date=short --pretty="format:%Cred%h%Cblue%d%Creset %s%n %Cgreen %ad, %an <%ce>%n%Cblue………………………………………………………………………………………………………………………………………………………………"'
 git config --global alias.m merge
 git config --global alias.s 'status -s'
 
