0f30256f50f7d55bb1c7a08104047de9a23f7c05
Jacob Hume <jacob.hume@tylertech.com>
2021-09-07 12:01:44 -0700

    Add alias for "git switch"

--- git-aliases.sh
+++ git-aliases.sh
@@ -15,6 +15,7 @@ git config --global alias.f 'fetch --all'
 git config --global alias.l 'log --graph --date=short --pretty="format:%Cred%h%Cblue%d%Creset %s%n %Cgreen %ad, %an <%ce>%n%Cblue………………………………………………………………………………………………………………………………………………………………"'
 git config --global alias.m merge
 git config --global alias.s 'status -s'
+git config --global alias.sw 'switch'
 
 
 ##
