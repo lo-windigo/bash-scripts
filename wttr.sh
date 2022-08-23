8f09345ea0bb4ae7f6f7db5f405e2ff175b6bdc7
Windigo <jacob@fragdev.com>
2022-08-23 10:42:55 -0700

    wttr.sh - futz around with defaults

--- wttr.sh
+++ wttr.sh
@@ -2,7 +2,7 @@
 # Get a weather forecast for our local area
 ##
 
-ZIP="${1:-95360}"
+ZIP="${1:-93955}"
 
 # Set up the request URL. See wttr.in/:help for more.
 # Flags:
@@ -10,11 +10,11 @@ ZIP="${1:-95360}"
 
 # Full forecast flag
 # 0 - Only current weather
-FORECAST=
+FORECAST='2'
 if [ "$( basename "$0" )" != 'forecast' ]; then
 	FORECAST='0'
 fi
 
-REQUEST="wttr.in/${ZIP}?${FORECAST}Q"
+REQUEST="wttr.in/${ZIP}?${FORECAST}"
 
 curl -H "Accept-Language: ${LANG%_*}" --compressed "$REQUEST"
