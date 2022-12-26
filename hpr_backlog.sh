8ec69aaf7599bc895a9c3f63ceeb6affba506f02
Windigo <jacob@fragdev.com>
2022-12-26 11:12:59 -0800

    Add HPR backlog script ;

--- hpr_backlog.sh
+++ hpr_backlog.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-SHOW_URI='http://hackerpublicradio.org/local'
+#set -x
+
+SHOW_URI='http://hackerpublicradio.org/eps'
 if [ -z "$HPR_FOLDER" ]; then
-	HPR_FOLDER="${HOME}/podcasts/hpr"
+	HPR_FOLDER="/srv/audio/podcasts/hpr"
 fi
 if [ -z "$SYNC_FOLDER" ]; then
-	SYNC_FOLDER="${HOME}/podcasts/sync"
+	SYNC_FOLDER="${HOME}/opt/mashpodder/new"
 fi
 
 # Calculate the latest show in the folder
-LATEST=$( \ls "${HPR_FOLDER}/"*.ogg | sort -r | head -n 1 )
+LATEST=$( find "${HPR_FOLDER}/" -type f -name '*.ogg' | sort -r | head -n 1 )
 
 if [ -z "$LATEST" ]; then
 	echo "Error getting latest HPR episode!"
@@ -19,6 +21,16 @@ fi
 # Start at the latest episode, and start counting down
 LATEST_FILE=$( basename "$LATEST" )
 SHOW_NUMBER="${LATEST_FILE:3:4}"
+
+# Use temporary files to prevent partial episodes from being written
+TEMP_SHOW_FILE=$( mktemp )
+function cleanup_temp_file {
+	if [ -e "$TEMP_SHOW_FILE" ]; then
+		rm "$TEMP_SHOW_FILE"
+	fi
+}
+
+trap "cleanup_temp_file" EXIT 
 
 # Check to see, sequentially, which files exist
 while [ "$SHOW_NUMBER" -gt 0 ]; do
@@ -36,14 +48,16 @@ while [ "$SHOW_NUMBER" -gt 0 ]; do
 			sleep 6s
 		fi
 
-		if curl --limit-rate 50K -s "${SHOW_URI}/${SHOW_FILE}" > "$SHOW_W_PATH"; then
+		if curl -L --limit-rate 50K -s "${SHOW_URI}/${SHOW_FILE}" > "$TEMP_SHOW_FILE"; then
 
 			# If this is an old MP3 episode, remove the mp3
 			if [ -e "$MP3_SHOW_FILE" ]; then
 				rm "$MP3_SHOW_FILE" 
 			fi
 
-			ln "$SHOW_W_PATH" "${SYNC_FOLDER}/${SHOW_FILE}"
+			mv "$TEMP_SHOW_FILE" "$SHOW_W_PATH"
+
+			cp "$SHOW_W_PATH" "${SYNC_FOLDER}/${SHOW_FILE}"
 		else
 			echo "ERROR: Show download failed!"
 			exit 404
@@ -55,3 +69,4 @@ while [ "$SHOW_NUMBER" -gt 0 ]; do
 	let "SHOW_NUMBER=SHOW_NUMBER-1"
 done
 
+#set +x
