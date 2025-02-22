5f611207c18ae88646ad380d634ffe76ac3a270f
Windigo <jacob@fragdev.com>
2025-02-22 08:38:50 -0800

    Brutalist shuffling media player, using mpv and bash

--- mpv_shuffle.sh
+++ mpv_shuffle.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+# Format for the "currently playing" line
+FMT='General;%Title%, by %Artist% on %Album%'
+OLD_IFS="$IFS"
+IFS=$'\n'
+MUSIC_DIR=~/Music
+
+if [ ! -z "$1" ]; then
+	MUSIC_DIR="$1"
+fi
+
+function fix_ifs {
+	IFS="$OLD_IFS"
+}
+
 # Ensure a non-image random song
 function random_song {
 	local CURRENTLY_PLAYING
 
 	while [ -z "$CURRENTLY_PLAYING" ]; do
-		CURRENTLY_PLAYING="$( random_file ~/Music )"
+		CURRENTLY_PLAYING="$( random_file "$MUSIC_DIR" )"
 
 		case $CURRENTLY_PLAYING in
 			*.ogg|*.flac|*.mp3|*.wma)
@@ -21,13 +35,16 @@ function random_song {
 	echo $CURRENTLY_PLAYING	
 }
 
+trap fix_ifs EXIT
+
 while true; do
 	CURRENTLY_PLAYING="$( random_song )"
 
+	mediainfo --Inform="$FMT" "$CURRENTLY_PLAYING"
+	echo "$CURRENTLY_PLAYING"
 	mpv --no-video --msg-level=all=no,statusline=status,term-msg=status \
-		--term-playing-msg='Currently Playing: ${media-title:Unknown} (${filename})' \
 		"$CURRENTLY_PLAYING"
 	echo 
-	sleep .5s
+	sleep .1s
 done
 
