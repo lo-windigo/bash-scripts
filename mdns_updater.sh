a4c1ef6f6c406415f0e39742e60e50f5bb353171
Windigo <jacob@fragdev.com>
2025-02-23 19:43:34 -0800

    mdns_updater.sh: A script that checks whether SDF MDNS is correct, and updates it otherwise

--- mdns_updater.sh
+++ mdns_updater.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-if ! which dig 2> /dev/null; then
+if ! which dig 2>&1 > /dev/null; then
 	echo '"dig" package required'
 	exit 1
 fi
 
-function mdns_ip {
-	dig @8.8.8.8 windigo.mdns.org +short 
-}
-
 WAN_IPV4=$( dig @resolver4.opendns.com myip.opendns.com +short )
 #WAN_IPV6=$( dig @ns1.google.com TXT o-o.myaddr.l.google.com +short -6 )
-MDNS_IP=$( mdns_ip ) 
+MDNS_IP=$( dig @8.8.8.8 windigo.mdns.org +short ) 
 
 if [ "$WAN_IPV4" != "$MDNS_IPV4" ]; then
 	ssh sdf "mdns $WAN_IPV4"
+	echo "Updating mdns with $WAN_IPV4"
+else
+	echo 'IP matches; no update needed'
 fi
