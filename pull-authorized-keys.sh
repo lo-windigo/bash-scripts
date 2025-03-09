9031c6cbb168c626affb17f4d55e2d67182f2b7e
Windigo <jacob@fragdev.com>
2025-03-09 06:01:59 +0000

    pull-authorized-keys.sh - Switch to pihole for SSH keys

--- pull-authorized-keys.sh
+++ pull-authorized-keys.sh
@@ -4,7 +4,7 @@ DEBUG=1
 TMP_AUTH_KEYS="${TMP:-/tmp}/authorized_keys.tmp"
 DEST_AUTH_KEYS="${HOME:-.}/.ssh/authorized_keys"
 
-ssh windigo@tty.sdf.org '$HOME/bin/public_keys' > "$TMP_AUTH_KEYS"
+ssh windigo@rapture.fragdev.net 'cat $HOME/ssh_keys/*' > "$TMP_AUTH_KEYS"
 
 if [ -s $TMP_AUTH_KEYS ]; then
 	mv "$DEST_AUTH_KEYS" "${DEST_AUTH_KEYS}.old"
