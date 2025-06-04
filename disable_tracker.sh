2d8a0e2c485dde69978c8fc8f47a4354dfc29b77
Windigo <jacob@fragdev.com>
2025-06-03 22:09:55 -0700

    disable_tracker.sh: Switch to newer, more interesting errors

--- disable_tracker.sh
+++ disable_tracker.sh
@@ -2,17 +2,18 @@
 #
 # Disable the ridiculous Gnome tracker 3 processes
 #
-
 if [ "$(/usr/bin/id -u)" -eq 0 ]; then
 	# Disable system components if running as root
-	systemctl --global disable tracker-miner-fs-3.service
-
-	tracker3 reset -s -r
+	if systemctl --global status tracker-miner-fs-3.service > /dev/null; then
+		dbus-launch systemctl --global disable tracker-miner-fs-3.service
+	fi
 else
 	# Disable user components if running as a normal user
-	systemctl --user stop tracker-miner-fs-3.service
-	systemctl --user mask tracker-miner-fs-3.service
-
-	tracker3 reset -s -r
+	if systemctl --user status tracker-miner-fs-3.service > /dev/null; then
+		dbus-launch systemctl --user stop tracker-miner-fs-3.service
+		dbus-launch systemctl --user mask tracker-miner-fs-3.service
+	fi
 fi
+
+dbus-launch tracker3 reset -s -r > /dev/null
 
